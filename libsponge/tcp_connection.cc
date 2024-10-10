28448ef2326562b6cdc30071b188700f1c98f421
ASENNIU <leonoro@163.com>
2024-10-10 22:11:35 +0800

    Initialize lab4

---
+++ libsponge/tcp_connection.cc
@@ -0,0 +1,21 @@
+#include "tcp_connection.hh"
+
+#include <iostream>
+
+// Dummy implementation of a TCP connection
+
+// For Lab 4, please replace with a real implementation that passes the
+// automated checks run by `make check`.
+
+template <typename... Targs>
+void DUMMY_CODE(Targs &&.../* unused */) {}
+
+using namespace std;
+
+size_t TCPConnection::remaining_outbound_capacity() const { return _sender.stream_in().remaining_capacity(); }
+
+size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
+
+size_t TCPConnection::unassembled_bytes() const { return _receiver.unassembled_bytes(); }
+
+size_t TCPConnection::time_since_last_segment_received() const { return _time_since_last_segment_received_counter; }
